d368065c7695a14a6b389c347c46eca7fb2fffd6
dev kushwaha <dev.kushwaha.7983@gmail.com>
2024-03-10 06:39:24 +0000

    operations

--- basic/03_coversionOperation.js
+++ basic/03_coversionOperation.js
@@ -8,9 +8,35 @@ let booleanIsLoogedIn = Boolean(isLoggedIn)
 // "" => false
 // "name" => true
 
-console.log(booleanIsLoogedIn)
+// console.log(booleanIsLoogedIn)
 
 let someNUmber = 33
 let numberString = String(someNUmber);
-console.log(numberString)
+// console.log(typeof numberString)
+// console.log(someNUmber);
+
+// ************************ Operations ***********************
+
+let value =3
+let negValue= -value;
+// console.log(negValue);
+
+// console.log(2**2) 2^2
+
+
+let str1 ='dev'
+let str2 = ' kushwaha';
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log('1'+2);
+// console.log(1+'2');
+// console.log('1'+'2');
+// console.log(+true);->1
+// console.log(+"");->0
+
+
+
+
+
 
