89425a17bd6d441951f697ccbb6e2cf42812b805
dev kushwaha <dev.kushwaha.7983@gmail.com>
2024-03-10 01:26:12 +0000

    converstion of data types

---
+++ basic/03_coversionOperation.js
@@ -0,0 +1,16 @@
+let score = "23" 
+
+let valueInNumber =  Number(score)
+
+let isLoggedIn = 1
+
+let booleanIsLoogedIn = Boolean(isLoggedIn)
+// "" => false
+// "name" => true
+
+console.log(booleanIsLoogedIn)
+
+let someNUmber = 33
+let numberString = String(someNUmber);
+console.log(numberString)
+
