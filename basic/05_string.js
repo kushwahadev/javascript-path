f84521f5215ece19005625156d60ee82beaa4826
dev kushwaha <dev.kushwaha.7983@gmail.com>
2024-03-11 06:33:52 +0000

    string introduction

---
+++ basic/05_string.js
@@ -0,0 +1,22 @@
+const name = 'dev'
+const repo = 50;
+
+
+// console.log(` my name is. ${name.toUpperCase()} and my repo count is ${repo}`)
+
+const gamename = new String('dev-gamming')
+
+
+// console.log(gamename[0])
+// console.log(gamename.__proto__)
+// console.log(gamename.lenght)
+// console.log(gamename.toUpperCase())
+// console.log(gamename.indexOf('m'))
+
+const newString = gamename.substring(0,4)
+
+const anotherString = gamename.slice(-8,4)
+
+const newStringOne = ' dev      '
+
+console.log(newStringOne.trim())
