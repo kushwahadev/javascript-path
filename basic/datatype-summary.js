b3b9be79658d971fea2d68c257735a36141e8399
dev kushwaha <dev.kushwaha.7983@gmail.com>
2024-03-11 05:40:39 +0000

    satck and heap concept added

--- basic/datatype-summary.js
+++ basic/datatype-summary.js
@@ -33,3 +33,31 @@ const myFun = function(){
 console.log('helloooooooo')
 }
 console.log(myFun)
+
+
+
+
+//     +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack(primitive).     heap (Non Primitive)
+
+
+let youtubename = 'this is name'
+// stack not update the orignal value make a copy and always update copy values
+let anothername = youtubename
+anothername =  ' this is another name'
+
+
+
+// what ever goes in heap always update the orignal value
+let userone = {
+    email : 'userone@gmail.com',
+    upi : 'user@ybl'
+}
+
+
+let usetwo = userone
+
+
+usetwo.email = 'dev@google.com'
+
