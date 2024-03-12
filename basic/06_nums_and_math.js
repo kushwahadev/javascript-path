9fcf443429dcc87babf3a8421024a40ae6b71c8d
dev kushwaha <dev.kushwaha.7983@gmail.com>
2024-03-12 16:56:28 +0000

    numbers and math

---
+++ basic/06_nums_and_math.js
@@ -0,0 +1,38 @@
+const balance = new Number(100)
+// console.log(balance)
+
+
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));
+
+
+const otherNumber  = 23.8966
+
+// console.log(otherNumber.toPrecision(3))
+
+const hundred = 1000000
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++   MATHS ++++++++++++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.3));
+// console.log(Math.max(4,3,1,8,9,20,2,5));
+// console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min =10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min);
+
+
+
