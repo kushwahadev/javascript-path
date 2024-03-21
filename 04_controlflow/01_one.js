8ed283fdd4273993093d93ff6d5c047ff34e0b70
Dev Kushwaha <devkushwaha3575@gmail.com>
2024-03-21 14:31:46 +0530

    conditionals intro

---
+++ 04_controlflow/01_one.js
@@ -0,0 +1,38 @@
+// if
+
+const isUserloggedIn = true;
+
+// < , > , <= , >= , == , != , ===
+
+if (2 === "2") {
+  console.log(`working`);
+}
+const temp = 41;
+
+// if (temp < 41) {
+//   console.log(`cold`);
+// } else {
+//   const heat = "melting";
+//   console.log(`this is ${heat} point `);
+// }
+
+const balance = 10000;
+// if (balance < 500) {
+//   console.log(`greater than 500`);
+// } else if (balance < 1000) {
+//   console.log(`greater than 1000`);
+// } else if (balance < 9000) {
+//   console.log(`greater than 9000`);
+// } else {
+//   console.log(`${balance} greater `);
+// }
+
+// switch casesss ------
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
