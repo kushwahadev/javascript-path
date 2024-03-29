62663eb8adc3c9b47ea5f9e2ffe18b5b0e11c48a
Dev Kushwaha <devkushwaha3575@gmail.com>
2024-03-29 19:51:46 +0530

    javascript  dom basics

---
+++ 06_dom/01_one.js
@@ -0,0 +1,24 @@
+//--------->>>> Run on console only
+// console.log(window);
+// console.dir(window);
+document.getElementById("title").id;
+document.getElementById("title").className;
+document.getElementById("title").getAttribute("id");
+title.style.backgroundColor = "teal";
+
+document.querySelector('input[type = "password"]');
+document.querySelector("ul");
+const myul = document.querySelector("ul");
+myul.querySelector("li");
+
+const turnGreen = myul.querySelector("li");
+turnGreen.style.backgroundColor = "green";
+turnGreen.style.padding = "15px";
+document.querySelectorAll("li");
+
+const tempLiListOne = document.querySelectorAll("li");
+tempLiList[0].style.color = "green";
+
+/// convert into array to use the array properties
+const tempLiListTwo = document.getElementsByClassName("list-item");
+Array.from(tempLiListTwo);
