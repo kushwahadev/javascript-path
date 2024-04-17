59d6731715e3c57f592e9d01c33de89a70517d41
Dev Kushwaha <devkushwaha3575@gmail.com>
2024-04-17 13:51:51 +0530

    format syntax

--- 01_basics/01_ariables.js
+++ 01_basics/01_ariables.js
@@ -1,8 +1,6 @@
-const accountID = 144553
-let accountEmail = "devkushwaha3575@gmail.com"
-var accountPassword = "12345"
-accountCity = 'Jaipur'
+const accountID = 144553;
+let accountEmail = "devkushwaha3575@gmail.com";
+var accountPassword = "12345";
+let accountCity = "Jaipur";
 
-
-console.table([accountID,accountEmail,accountPassword,accountCity])
-
+console.table([accountID, accountEmail, accountPassword, accountCity]);
